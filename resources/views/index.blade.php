6ee297898c7a153eae74045462f0986dab952eb6
Quynh <bachcongtu2904@gmail.com>
2020-12-16 10:42:52 +0700

    update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,71 @@
+<form method="post" style="position: absolute;left: 40%;top:100px">
+<table>
+        <tr>
+            <input type="text" id="number" style="height: 30px;width: 150px">
+        </tr>
+        <tr>
+            <td>
+                <button type="submit" value="7">7</button>
+            </td>
+            <td>
+                <button type="submit" value="8">8</button>
+            </td>
+            <td>
+                <button type="submit" value="9">9</button>
+            </td>
+            <td>
+                <button type="submit" value="/">/</button>
+            </td>
+            <td>
+                <button type="submit" value="C">CE</button>
+            </td>
+        </tr>
+    <tr>
+        <td>
+            <button type="submit" value="4">4</button>
+        </td>
+        <td>
+            <button type="submit" value="5">5</button>
+        </td>
+        <td>
+            <button type="submit" value="6">6</button>
+        </td>
+        <td>
+            <button type="submit" value="*">*</button>
+        </td>
+        <td>
+            <button type="submit" value="C">C</button>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <button type="submit" value="1">1</button>
+        </td>
+        <td>
+            <button type="submit" value="2">2</button>
+        </td>
+        <td>
+            <button type="submit" value="3">3</button>
+        </td>
+        <td>
+            <button type="submit" value="-">-</button>
+        </td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>
+            <button type="submit" value="0">0</button>
+        </td>
+        <td></td>
+        <td>
+            <button type="submit" value=".">.</button>
+        </td>
+        <td>
+            <button type="submit" value="+">+</button>
+        </td>
+        <td>
+            <button type="submit" value="=">=</button>
+        </td>
+    </tr>
+</table>
+</form>
